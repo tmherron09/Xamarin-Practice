fc2a840d9da108f831d91d5682a47a6ba56fbff3
Timothy M Herron <tmherron09@gmail.com>
2021-07-14 10:14:17 -0500

    Add MainPageViewModel

---
+++ learn_dotNetXAML_01/learn_dotNetXAML_01/ViewModels/MainPageViewModel.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace learn_dotNetXAML_01.ViewModels
+{
+    public class MainPageViewModel : INotifyPropertyChanged
+    {
+
+        public MainPageViewModel()
+        {
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ObservableCollection<string> AllNotes { get; set; }
+
+        string theNote;
+
+        public string TheNote
+        {
+            get => theNote;
+            set
+            {
+                theNote = value;
+                var args = new PropertyChangedEventArgs(nameof(TheNote));
+
+                PropertyChanged?.Invoke(this, args);
+            }
+        }
+
+        Command SaveCommand { get; }
+        Command EraseCommand { get; }
+
+
+    }
+}
