e7f9bac5136d37b8f3b05d01a4e461249f03f38c
Timothy M Herron <tmherron09@gmail.com>
2021-07-14 11:23:06 -0500

    Add Navigation to DetailPage with DismissCommand

---
+++ learn_dotNetXAML_01/learn_dotNetXAML_01/ViewModels/DetailPageViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Xamarin.Forms;
+
+namespace learn_dotNetXAML_01.ViewModels
+{
+    public class DetailPageViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DetailPageViewModel(string note)
+        {
+            NoteText = note;
+
+            DismissPageCommand = new Command(async () =>
+            {
+                await Application.Current.MainPage.Navigation.PopAsync();
+            });
+        }
+
+        string noteText;
+        public string NoteText { 
+            get => noteText; 
+            set {
+                noteText = value;
+                var args = new PropertyChangedEventArgs(nameof(NoteText));
+
+                PropertyChanged?.Invoke(this, args);
+            } 
+        }
+
+        public Command DismissPageCommand { get; }
+
+    }
+}
--- learn_dotNetXAML_01/learn_dotNetXAML_01/ViewModels/MainPageViewModel.cs
+++ learn_dotNetXAML_01/learn_dotNetXAML_01/ViewModels/MainPageViewModel.cs
@@ -9,25 +9,6 @@ namespace learn_dotNetXAML_01.ViewModels
 {
     public class MainPageViewModel : INotifyPropertyChanged
     {
-
-        public MainPageViewModel()
-        {
-
-            AllNotes = new ObservableCollection<string>();
-
-            EraseCommand = new Command(() =>
-            {
-                TheNote = string.Empty;
-            });
-
-            SaveCommand = new Command(() =>
-            {
-                AllNotes.Add(TheNote);
-
-                TheNote = string.Empty;
-            });
-        }
-
         public event PropertyChangedEventHandler PropertyChanged;
 
         public ObservableCollection<string> AllNotes { get; set; }
@@ -46,8 +27,55 @@ namespace learn_dotNetXAML_01.ViewModels
             }
         }
 
+        string selectedNote;
+        public string SelectedNote {
+            get => selectedNote;
+            set
+            {
+                selectedNote = value;
+
+            }
+        }
+
         public Command SaveCommand { get; }
         public Command EraseCommand { get; }
+        public Command SelectedNoteChangedCommand { get; }
+
+
+        public MainPageViewModel()
+        {
+
+            AllNotes = new ObservableCollection<string>();
+
+            EraseCommand = new Command(() =>
+            {
+                TheNote = string.Empty;
+            });
+
+            SaveCommand = new Command(() =>
+            {
+                AllNotes.Add(TheNote);
+
+                TheNote = string.Empty;
+            });
+
+            SelectedNoteChangedCommand = new Command(async () =>
+            {
+                var detailedVM = new DetailPageViewModel(SelectedNote);
+
+                var detailPage = new DetailPage();
+
+                detailPage.BindingContext = detailedVM;
+
+                await Application.Current.MainPage.Navigation.PushAsync(detailPage);
+
+            });
+
+        }
+
+        
+
+        
 
 
     }
